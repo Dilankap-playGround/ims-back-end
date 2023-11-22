36f8ac511d72d597034f064cc68a61f60e9d1f53
DilankaSandeep <dilankacm@gmail.com>
2023-11-22 15:13:14 +0530

    add to schema.sql new

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,3 +9,10 @@ CREATE TABLE IF NOT EXISTS course(
     name VARCHAR(100) NOT NULL,
     duration_in_months INT NOT NULL
 );
+CREATE TABLE IF NOT EXISTS teacher_course(
+                                             teacher_id INT NOT NULL,
+                                             course_id INT NOT NULL,
+                                             CONSTRAINT PRIMARY KEY (teacher_id, course_id),
+                                             CONSTRAINT fx_1 FOREIGN KEY (teacher_id) REFERENCES teacher(id),
+                                             CONSTRAINT fx_2 FOREIGN KEY (course_id) REFERENCES course(id)
+);
