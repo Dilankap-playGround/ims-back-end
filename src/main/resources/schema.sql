96a4d1f546f1889ac974f6bbd1ab06747db60f78
DilankaSandeep <dilankacm@gmail.com>
2023-11-22 16:47:30 +0530

    test commit 2

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS teacher_course(
      teacher_id INT NOT NULL,
      course_id INT NOT NULL,
      CONSTRAINT PRIMARY KEY (teacher_id, course_id),
-    CONSTRAINT fx_1 FOREIGN KEY (teacher_id) REFERENCES teacher(id),
+        CONSTRAINT fx_1 FOREIGN KEY (teacher_id) REFERENCES teacher(id),
       CONSTRAINT fx_2 FOREIGN KEY (course_id) REFERENCES course(id)
 );
 
